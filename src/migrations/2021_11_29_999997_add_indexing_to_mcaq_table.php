02a88e17ef5ae5fecb22d74445685381919ca7a2
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 12:34:33 +0530

    mcaq

---
+++ src/migrations/2021_11_29_999997_add_indexing_to_mcaq_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMcaqAnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('fmt_mcaq_ans')) {
+            Schema::table('fmt_mcaq_ans', function (Blueprint $table) {
+                $table->index('question_id');
+                $table->index('active');
+                $table->index('media_id');
+                $table->index('arrange');
+            });
+        }
+        if (Schema::hasTable('fmt_mcaq_ques')) {
+            Schema::table('fmt_mcaq_ques', function (Blueprint $table) {
+                $table->index('active');
+                $table->index('media_id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('fmt_mcaq_ans');
+    }
+}
